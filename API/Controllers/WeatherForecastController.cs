37652dff18f0d691bbbfac46f09a31489bf5e96c
Amdemichael Kidane <amkidane@excellerentsolutions.com>
2022-12-15 13:06:08 +0300

    Initial commit

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _context;
+         private readonly ILogger<WeatherForecastController> _logger;
+        public WeatherForecastController(DataContext context, ILogger<WeatherForecastController> logger)
+        {
+            _context = context;
+            _logger = logger;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+           return  Ok( await _context.Values.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
+        }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {   
+
+        }
+
+         public DbSet<Value> Values { get; set; }
+
+         protected override void OnModelCreating(ModelBuilder builder)
+         {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id=1, Values="Value 101"},
+                    new Value {Id=2, Values="Value 102"},
+                    new Value {Id=3, Values="Value 103"}
+                );
+         }
+    }
+}
